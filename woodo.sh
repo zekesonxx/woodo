0a4f4259f03bdde0155879582d9d1cdfab7abdfd
Zach Mertes <zmertes@purdue.edu>
2019-08-17 21:44:47 -0400

    Initial Commit

---
+++ woodo.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$(whoami)" != "root" ]; then
+	echo "usage: sudo woodo";
+	exit 1;
+fi
+
+echo '     _              __'
+echo '    / `\  (~._    ./  )'
+echo '    \__/ __`-_\__/ ./'
+echo '   _ \ \/  \   \ |_   __'
+echo ' (   )  \__/ -^    \ /  \'
+echo '  \_/ "  \  | o  o  |.. /  __'
+echo "       \. --' ====  /  || /  \\"
+echo '         \   .  .  |---__.\__/'
+echo '         /  :     /   |   |'
+echo '         /   :   /     \_/'
+echo '      --/ ::    ('
+echo '     (  |     (  (____'
+echo '   .--  .. ----**.____)'
+echo '   \___/'
